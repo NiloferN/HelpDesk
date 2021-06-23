077fc0a0d0d349b9d662733bb455329242906276
NiloferN <nilofernargees@gmail.com>
2021-06-23 13:34:28 +0530

    Initial Commit

---
+++ Test/test_app.py
@@ -0,0 +1,17 @@
+from flask.app import Flask
+from app import home, index
+def test_base_route():
+    app = Flask(__name__)
+    home(app)
+    client = app.test_client()
+    url = '/'
+
+    response = client.get(url)
+    assert response.get_data() == b'Login'
+    assert response.status_code == 200
+    
+#from app import index
+
+
+#def test_index():
+ #   assert index() == "Hello, world!"
